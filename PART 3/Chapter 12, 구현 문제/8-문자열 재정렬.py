ae38768ad8fd59c141f8ea3d01af0f8378f89dcb
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-24 10:00:42 +0900

    [Update] Chapter 12, 구현 문제

--- PART 3/Chapter 12, 구현 문제/8-문자열 재정렬.py
+++ PART 3/Chapter 12, 구현 문제/8-문자열 재정렬.py
@@ -3,10 +3,13 @@ ans = []
 cnt = 0
 
 for i in s:
+    # 숫자면 cnt에 더하기
     if i.isdigit():
         cnt += int(i)
+    # 문자면 ans에 넣기
     elif i.isalpha():
         ans.append(i)
+# 오름차순 정렬
 ans.sort()
 ans.append(str(cnt))
 
